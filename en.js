46563dd4fbc3888f7156c014161f94db92569dd2
Mapz <61966578@qq.com>
2016-10-21 18:13:59 +0800

    first commit

---
+++ en.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"1":"English1",
+	"2":"English2",
+	"3":"English3",
+	"4":"English4",
+	"5":"English5",
+	"6":"23eed",
+	"7":"",
+}
---
+++ test_main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/andlabs/ui"
+	"github.com/tealeg/xlsx"
+)
+
+var logText string
+var logArea *ui.Label
+
+func main() {
+	logText = ""
+
+	err := ui.Main(func() {
+		button := ui.NewButton("选择需要转化的文件")
+		logArea = ui.NewLabel("")
+		box := ui.NewVerticalBox()
+		box.Append(button, false)
+		box.Append(logArea, false)
+		window := ui.NewWindow("文字表转换工具 XLS --> JSON对象", 400, 200, false)
+		window.SetChild(box)
+		button.OnClicked(func(*ui.Button) {
+			openFile := ui.OpenFile(window)
+			_err := processXLSX(openFile)
+			if _err != nil {
+				appendLog("进程出错:")
+				appendLog(_err.Error())
+			}
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func appendLog(str string) error {
+	logText += str + "\r\n"
+	logArea.SetText(logText)
+	return nil
+}
+
+func processXLSX(filePath string) error {
+	appendLog("选择了文件：" + filePath)
+	excelFileName := filePath // "/Users/mapzchen/Documents/golangEx/src/github.com/Mapz/guiTest/test_xls_text.xlsx"
+	dir, _ := path.Split(filePath)
+	fmt.Println("路径：" + dir)
+
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		return err
+	}
+	sheet := xlFile.Sheets[0] //读取第个sheet
+	//读取表头，顺便创建语言目录
+	headRow := sheet.Rows[0]
+	languageMap := make(map[int]string)
+
+	for colIndex, cell := range headRow.Cells[1:len(headRow.Cells)] {
+		_str, _err := cell.String()
+		if _err != nil {
+			return _err
+		}
+		appendLog("语言版本：" + _str)
+		languageMap[colIndex] = _str
+	}
+
+	fmt.Println(languageMap)
+
+	//创建导出文件
+	colFileMap := make(map[int]*os.File)
+	for col, ver := range languageMap {
+		userFile := ver + ".js"
+		_path := dir + userFile
+		fout, err := os.Create(_path)
+		appendLog("创建文件:" + _path)
+		defer fout.Close()
+		defer appendLog("完成导出:" + _path)
+		if err != nil {
+			fmt.Println(userFile, err)
+			return err
+		}
+		fout.WriteString("module.exports = {\r\n")
+		colFileMap[col] = fout
+
+	}
+
+	//开始读取数据
+	for _, row := range sheet.Rows[1:len(sheet.Rows)] {
+		_id, _err := row.Cells[0].String()
+		if _err != nil {
+			return _err
+		}
+		for col, cell := range row.Cells[1:len(row.Cells)] {
+			_str, _err := cell.String()
+			if _err != nil {
+				return _err
+			}
+			colFileMap[col].WriteString("\t\"" + _id + `":"` + _str + "\",\r\n")
+			fmt.Printf("%s    %s\n", _err, _str)
+		}
+	}
+
+	for _, file := range colFileMap {
+		file.WriteString("}")
+	}
+
+	return nil
+}
